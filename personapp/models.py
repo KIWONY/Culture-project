6b7c1b1cbadeebbc2c16585b1413f6554d7163bf
KIWONY <kien1004@naver.com>
2022-02-10 01:22:19 +0900

    make Models

--- personapp/models.py
+++ personapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class NiceWorld(models.Model):
+    text = models.CharField(max_length=255, null= False)
+
+    # 명령어 python manage.py makemigrations
+    #       python manage.py migrate
