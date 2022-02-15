cdb8a7bc9b5075b1987503c6336f8a464f0347c8
KIWONY <kien1004@naver.com>
2022-02-15 21:55:29 +0900

    profileapp implementation

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -7,3 +7,4 @@ class ProfileCreationForm(ModelForm):
     class Meta:                                         #장고 제공
         model = Profile
         fields = ["image", "nickname", "message"]           #models.py의 class Profile에서 설정한 field
+        #user를 field에 설정하지않은 이유: client에서가 아닌 서버에서 구현하여 본인이 아닌 user가 접근하여 이용하는 것을 막기위함
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = "target_profile"
+    form_class = ProfileCreationForm                                    #forms파일에서 생성했던.
+    success_url = reverse_lazy("personapp:nice_world")
+    template_name = "profileapp/create.html"
+
+
+
+    # 아래의 form은 forms.py에서 생성한 ProfileCreationForm
+    # form_valid 메서드는 유저가 보낸 모든 입력을 검증하고, 검증이 완료 된 이후 실행되는 메서드
+    def form_valid(self, form):
+        #커스텀마이징(오버라이딩?)
+        temp_profile = form.save(commit=False)      #commit을 False로 설정함으로써 실제 db에 반영이 되지않음.
+        temp_profile.user = self.request.user       #유저데이터를 request를 보낸 user로 정한다.
+        temp_profile.save()
+        return super().form_valid(form)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
