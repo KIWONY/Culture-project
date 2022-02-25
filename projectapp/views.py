bc14992ba811ddaf19c81c0315a266ef0a08c9bf
KIWONY <kien1004@naver.com>
2022-02-26 01:22:43 +0900

    projectapp with class

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,14 +1,16 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, ListView
 
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
-
-
+@method_decorator(login_required,"get")
+@method_decorator(login_required,"post")
 class ProjectCreateView(CreateView):
     model = Project
     template_name = "projectapp/create.html"
