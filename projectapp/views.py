24b9fe6476a8a80302cd033617b40c70833a52c2
KIWONY <kien1004@naver.com>
2022-02-27 03:08:35 +0900

    add Minxin for connection project detail and articles

--- projectapp/views.py
+++ projectapp/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic.list import MultipleObjectMixin
 
+from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
@@ -19,13 +21,18 @@ class ProjectCreateView(CreateView):
     def get_success_url(self):
         return reverse("projectapp:detail", kwargs={"pk" : self.object.pk})
 
-
-
-class ProjectDetailView(DetailView):
+#MultipleObjectMixin를 사용하여 다른 object들도 다루게 함
+class ProjectDetailView(DetailView, MultipleObjectMixin):
     model = Project
     template_name = "projectapp/detail.html"
     context_object_name = "target_project"
 
+    paginate_by = 20
+
+#해당 프로젝트에 해당하는 article들을 가져옴
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(project=self.get_object())
+        return super(ProjectDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 class ProjectListView(ListView):
     model = Project
