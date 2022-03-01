8ab15a4699530a9ad7bbf558e2ac1355af482e73
KIWONY <kien1004@naver.com>
2022-03-01 18:34:56 +0900

    add medium editor for WYSIWYG to ArticleCreateForm

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,13 @@
 from django.forms import ModelForm
+from django import forms
 
 from articleapp.models import Article
 
 
 class ArticleCreationForm(ModelForm):
+    #에디터 커스텀마이징
+    content = forms.CharField(widget=forms.Textarea(attrs={"class":"editable text-left","style":"height:auto"}))
+
     class Meta:
         model = Article
         fields = ["title", "project", "image", "content"]
