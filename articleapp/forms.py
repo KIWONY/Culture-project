4783ea34a8328ce23d130a7086775f10f2767249
KIWONY <kien1004@naver.com>
2022-03-01 22:35:06 +0900

    add choicefield to ArticleCreationform

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -2,12 +2,15 @@ from django.forms import ModelForm
 from django import forms
 
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
     #에디터 커스텀마이징
     content = forms.CharField(widget=forms.Textarea(attrs={"class":"editable text-left","style":"height:auto"}))
 
+    #foreignkey선택 시 선택하지 않아도 되게
+    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
     class Meta:
         model = Article
         fields = ["title", "project", "image", "content"]
