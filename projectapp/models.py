90358bcd27528a06b3ee194af0ee6e00cd20d541
KIWONY <kien1004@naver.com>
2022-03-02 02:58:13 +0900

    show project number and title when selecting

--- projectapp/models.py
+++ projectapp/models.py
@@ -12,3 +12,14 @@ class Project(models.Model):
     description = models.TextField(max_length=100, null=True)
 
     create_at = models.DateField(auto_now =True, null = False)
+
+    #project를 찾을 때 Project.pk값과 그 값에 해당하는 title이 순서대로 나오게 함
+    def __str__(self):
+        return f"{self.pk} : {self.title}"
+
+
+
+
+
+
+
