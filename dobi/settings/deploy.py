46a3203ceeb9b033b873ee9c1e809c96afd1f691
KIWONY <kien1004@naver.com>
2022-04-09 02:46:25 +0900

    fix deploy file to use docker secret

--- dobi/settings/deploy.py
+++ dobi/settings/deploy.py
@@ -1,5 +1,14 @@
 from .base import *
 
+#env파일이 아닌 Docker secrets에서 가져오는 함수
+def read_secret(secret_name):
+
+    file = open("/run/secrets/" + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()   #정제
+    file.close()
+
+    return secret
 
 environ.Env.read_env(
     env_file = os.path.join(BASE_DIR,'.env')
@@ -12,7 +21,8 @@ env = environ.Env(
 
 # pymysql.install_as_MySQLdb()
 #20210305 SECRET_KEY바꿈
-SECRET_KEY = env('SECRET_KEY')
+#함수 이용
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -27,7 +37,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'password1234',
+        'PASSWORD': read_secret("MYSQL_PASSWORD"),
         'HOST': 'mariadb',  #mariadb의 container이름
         'PORT': '3306',
     }
